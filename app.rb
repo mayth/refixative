7b20e2002a77be328561ff69b674d587b1d20960
mayth <chimeaquas@hotmail.com>
2012-12-04 06:12:54 +0900

    Ready to use Rack::Flash, fix Gemfile

--- app.rb
+++ app.rb
@@ -1,5 +1,11 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
+require 'rack/flash'
+require_relative './parser'
+
+configure do
+  use Rack::Session::Cookie, :secret => IO.read('cookie_secret')
+  use Rack::Flash
 
 get '/' do
   haml :index
@@ -7,4 +13,7 @@ end
 
 get '/register' do
   haml :register_form
+end
+
+post '/register' do
 end
