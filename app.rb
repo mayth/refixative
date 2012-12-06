a3f5118a6c26e01bbed39be46f7ee2a9701e6993
mayth <chimeaquas@hotmail.com>
2012-12-07 03:54:03 +0900

    Implement registraion form.
    * Implement registration form.
    * Use logger for Sequel.
    * Use hidden forms instead of Flash object.

--- app.rb
+++ app.rb
@@ -1,11 +1,19 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
-require 'rack/flash'
-require_relative './parser'
+require 'logger'
+require_relative 'parser'
+
+require_relative 'db_connect'
+
+DB.loggers << Logger.new(STDOUT)
+
+Dir.glob('./models/*.rb').each do |s|
+  puts "requires #{s}"
+  require_relative s
+end
 
 configure do
-  use Rack::Session::Cookie, :secret => IO.read('cookie_secret')
-  use Rack::Flash
+end
 
 get '/' do
   haml :index
@@ -16,4 +24,18 @@ get '/register' do
 end
 
 post '/register' do
+  halt 'profile file is not uploaded.' unless params[:profile]
+  halt 'music file is not uploaded.' unless params[:music]
+  parser = Parser::Colette.new
+  @prof = parser.parse_profile(params[:profile][:tempfile].read)
+  @song = parser.parse_song(params[:music][:tempfile].read)
+  haml :register_confirm
+end
+
+post '/registered' do
+  halt 'profile is not sent.' unless params[:prof]
+  halt 'music data is not sent.' unless params[:song]
+  @prof = params[:prof]
+  @song = params[:song]
+  haml :registered
 end
