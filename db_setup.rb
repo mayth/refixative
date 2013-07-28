123bfd6bbd5fad4c853a3b54ab6b4c0c32d453c6
mayth <chimeaquas@hotmail.com>
2013-07-29 02:25:32 +0900

    Do not require sinatra in db_setup

--- db_setup.rb
+++ db_setup.rb
@@ -1,9 +1,8 @@
 require 'logger'
-require 'sinatra'
 require 'pg'
 require 'sequel'
 require 'dalli'
 
 DB = Sequel.connect('postgres://refixative@localhost/refixative')
 CACHE = Dalli::Client.new('localhost:11215', namespace: 'rfx', compress: true)
-DB.loggers << Logger.new(STDOUT) if development?
+DB.loggers << Logger.new(STDOUT) if (respond_to?(:development?) ? development? : false)
