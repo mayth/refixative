200aa59e0b8d535ff2f2b4c6ca20ad29273aada0
mayth <chimeaquas@hotmail.com>
2013-10-30 10:43:08 +0900

    Add spec for Parser::name_normalize

--- spec/lib/parser_spec.rb
+++ spec/lib/parser_spec.rb
@@ -1,7 +1,28 @@
 require 'spec_helper'
 
+describe Parser do
+  describe '::name_normalize' do
+
+    context 'when given string which has no characters to replace' do
+      subject { Parser.name_normalize('abcd') }
+      it { should == 'abcd' }
+    end
+
+    context 'when given nil' do
+      it do
+        expect { Parser.name_normalize(nil) }.to raise_error
+      end
+    end
+
+    context 'when given string which has some characters to replace' do
+      subject { Parser.name_normalize("abc  de''f''") }
+      it { should == 'abc de"f"' }
+    end
+  end
+end
+
 describe Parser::Colette do
-  describe '#parse_profile' do
+  describe '::parse_profile' do
     context 'when given valid HTML file' do
       before do
         @file = File.open(File.join(__dir__, 'profile.html'))
@@ -27,7 +48,7 @@ describe Parser::Colette do
     end
   end
 
-  describe '#parse_music' do
+  describe '::parse_music' do
     context 'when given valid HTML file' do
       before do
         @file = File.open(File.join(__dir__, 'music.html'))
