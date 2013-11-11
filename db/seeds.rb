1ba618a6baa432749ef35569c092766f942b2d19
mayth <chimeaquas@hotmail.com>
2013-11-11 16:58:18 +0900

    Add seed data for versions

---
+++ db/seeds.rb
@@ -0,0 +1,6 @@
+Version.create(name: 'REFLEC BEAT', released_at: Time.local(2010, 11, 4))
+Version.create(name: 'limelight', released_at: Time.local(2011, 11, 16))
+Version.create(name: 'colette Winter', released_at: Time.local(2012, 11, 21))
+Version.create(name: 'colette Spring', released_at: Time.local(2013,  3, 18))
+Version.create(name: 'colette Summer', released_at: Time.local(2013,  6, 19))
+Version.create(name: 'colette Autumn', released_at: Time.local(2013,  9, 18))
