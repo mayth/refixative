3f4dffc4168c86d7d172dddaa26422c408bad148
mayth <chimeaquas@hotmail.com>
2014-01-13 01:39:54 +0900

    Add version data in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Note that the timezone of the released date is JST.
+Version.create(name: 'REFLEC BEAT',           released_at: Time.local(2010, 11,  4, nil, nil, nil, '+09:00'))
+Version.create(name: 'limelight',             released_at: Time.local(2011, 11, 16, nil, nil, nil, '+09:00'))
+Version.create(name: 'colette -Winter-',        released_at: Time.local(2012, 11, 21, nil, nil, nil, '+09:00'))
+Version.create(name: 'colette -Spring-',        released_at: Time.local(2013,  3, 18, nil, nil, nil, '+09:00'))
+Version.create(name: 'colette -Summer-',        released_at: Time.local(2013,  6, 19, nil, nil, nil, '+09:00'))
+Version.create(name: 'colette -Autumn-',        released_at: Time.local(2013,  9, 18, nil, nil, nil, '+09:00'))
+Version.create(name: 'colette All Seasons', released_at: Time.local(2013, 12, 11, nil, nil, nil, '+09:00'))
