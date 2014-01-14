7c6845a313b0ee2503eb2edba1736e00fb083523
mayth <chimeaquas@hotmail.com>
2014-01-15 03:12:54 +0900

    Set default timezone to Tokyo.

--- config/application.rb
+++ config/application.rb
@@ -8,15 +8,9 @@ Bundler.require(:default, Rails.env)
 
 module RefixativeNext
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
 
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
-
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     I18n.enforce_available_locales = true
     config.i18n.default_locale = :ja
