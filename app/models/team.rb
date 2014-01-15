0437fc3ce45a4bf5aae90dad67ece56150744eb8
mayth <chimeaquas@hotmail.com>
2014-01-15 18:52:40 +0900

    Add validations for team model.

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,8 @@
 class Team < ActiveRecord::Base
   self.primary_key = :id
   has_many :players
+
+  validates :id,
+    presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :name, presence: true, length: { maximum: 8 }
 end
