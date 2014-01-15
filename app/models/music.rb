5c7550919a140fb64c595d92ef270e9ec986a4c3
mayth <chimeaquas@hotmail.com>
2014-01-15 18:39:21 +0900

    Add validations for music model.

--- app/models/music.rb
+++ app/models/music.rb
@@ -1,3 +1,8 @@
 class Music < ActiveRecord::Base
   belongs_to :version
+
+  validates :name, presence: true
+  validates :basic_lv, :medium_lv, :hard_lv,
+    numericality: true, inclusion: { in: 1..10 }
+  validates :added_at, presence: true
 end
