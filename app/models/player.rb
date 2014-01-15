d3920652b8e4e9de0e0b4f7cd7e0bc97a6718718
mayth <chimeaquas@hotmail.com>
2014-01-15 18:45:07 +0900

    Add validations for player model.

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,4 +2,14 @@ class Player < ActiveRecord::Base
   self.primary_key = :id
   has_many :scores
   belongs_to :team
+
+  validates :id,
+    presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :name, presence: true, length: { maximum: 8 }
+  validates :pseudonym, presence: true
+  validates :comment, length: { maximum: 16 }
+  validates :play_count, :stamp, :onigiri,
+    numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :last_play_date, presence: true
+  validates :last_play_shop, presence: true
 end
