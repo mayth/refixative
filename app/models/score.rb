2b839cfbc69ee208592f993911f6ceb6edfca903
mayth <chimeaquas@hotmail.com>
2014-01-15 18:52:30 +0900

    Add validations for score model.

--- app/models/score.rb
+++ app/models/score.rb
@@ -2,4 +2,8 @@ class Score < ActiveRecord::Base
   has_many :records
   belongs_to :player
   belongs_to :music
+
+  validates_associated :player
+  validates_associated :music
+  validates :difficulty, presence: true, inclusion: { in: 0..2 }
 end
