6fde03b3536d9c9ff8f24dcbb9eb10c6ad26d73c
mayth <chimeaquas@hotmail.com>
2014-01-15 18:47:52 +0900

    Add validations for record model.

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,3 +1,7 @@
 class Record < ActiveRecord::Base
   belongs_to :score
+
+  validates_associated :score
+  validates :achievement, numericality: true
+  validates :miss_count, numericality: true
 end
