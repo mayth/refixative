c52aac236206b484d76df87e1722b7c654ed65fd
mayth <chimeaquas@hotmail.com>
2014-01-17 04:21:59 +0900

    Add Scores#register

---
+++ app/controllers/scores_controller.rb
@@ -0,0 +1,4 @@
+class ScoresController < ApplicationController
+  def register
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 RefixativeNext::Application.routes.draw do
+  get "scores/register"
   get "home/index"
   root 'home#index'
 end
