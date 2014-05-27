1efb5020e1d6abea9a3dc951da90f27e7be35d4a
mayth <chimeaquas@hotmail.com>
2014-05-27 23:56:55 +0900

    Specify listen port, std(err|out) path absolutely, and pidfile.

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,10 +1,16 @@
+app_root = ENV['RAILS_ROOT'] || File.expand_path('.')
+
 worker_processes Integer(ENV['WEB_CONCURRENCY'] || 4)
 timeout 15
 preload_app true
 GC.copy_on_write_friendly = true if GC.respond_to?(:copy_on_write_friendly=)
 
-stderr_path 'log/unicorn.log'
-stdout_path 'log/unicorn.log'
+listen 9382
+
+stderr_path File.join(app_root, 'log', 'unicorn.log')
+stdout_path File.join(app_root, 'log', 'unicorn.log')
+
+pid File.join(app_root, 'shared', 'pids', 'unicorn.pid')
 
 before_fork do |server, worker|
   ActiveRecord::Base.connection.disconnect! if defined?(ActiveRecord::Base)
