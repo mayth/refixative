107d85c4d37e72099ffff4be2bae989740e8c1cd
mayth <chimeaquas@hotmail.com>
2014-06-04 03:51:45 +0900

    Accepts the value which is included within 0.0-100.0 as Record#achieve

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,7 +1,8 @@
 class Record < ActiveRecord::Base
   belongs_to :score, inverse_of: :records
   structure do
-    achieve    90.0, validates: [:presence, :numericality]
+    achieve    90.0,
+      validates: [:presence, :numericality, inclusion: { in: 0.0..100.0 } ]
     miss       3, validates: [
       :presence,
       numericality: { only_integer: true, greater_than_or_equal_to: 0 }
