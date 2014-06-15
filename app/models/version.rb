c385f34157b496bf719f4d9d830939be18de38a4
mayth <chimeaquas@hotmail.com>
2014-06-16 05:16:33 +0900

    Use `'` instead of `"' if it is no need to use interpolation.

--- app/models/version.rb
+++ app/models/version.rb
@@ -1,7 +1,7 @@
 class Version < ActiveRecord::Base
   has_many :musics, inverse_of: :version
   structure do
-    name "colette Winter", validates: :presence
+    name 'colette Winter', validates: :presence
     released_at Time.new(2012, 11, 21, 10, 0, 0, '+09:00'), validates: :presence
     timestamps
   end
